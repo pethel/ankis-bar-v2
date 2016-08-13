fc53ae20e3a30043106680667aa7baea267b4b26
Peter Hellstrand <phe@capraconsulting.no>
2016-08-13 18:22:28 +0200

    locale is stored

--- src/main/resources/site/pages/default/default.js
+++ src/main/resources/site/pages/default/default.js
@@ -1,9 +1,7 @@
-var thymeleaf = require('/lib/xp/thymeleaf');
+
 
 exports.post  = function(req) {
     var portal = require('/lib/xp/portal');
-    log.info('params === %s', req.params);
-
     return {
         redirect: portal.pageUrl({id: portal.getSite()._id}),
         cookies: {
@@ -13,22 +11,20 @@ exports.post  = function(req) {
 
 };
 
-exports.get = function(req) {
+exports.get = function() {
 
-    // Resolve the view
+    var thymeleaf = require('/lib/xp/thymeleaf');
+
     var view = resolve('default.html');
     var portal = require('/lib/xp/portal');
-    // Define the model
-    var content = portal.getContent();
 
+    var content = portal.getContent();
     var mainRegion = content.page.regions["main"];
 
     var model = {mainRegion: mainRegion};
 
-    // Render a thymeleaf template
     var body = thymeleaf.render(view, model);
 
-    // Return the result
     return {
         body: body,
         contentType: 'text/html'
