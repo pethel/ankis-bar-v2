c4cb560c2ea56c38e752cf1304682941306877c3
Peter Hellstrand <phe@capraconsulting.no>
2016-08-21 09:26:11 +0200

    menu working picking up ingredients

---
+++ src/main/resources/lib/data.js
@@ -0,0 +1,65 @@
+/**
+ * Force data to array
+ * Note that current UTIL.log function won't reflect the changes due to a bug in JSON.stringify
+ * @param data
+ * @returns {Array}
+ */
+exports.forceArray = function(data) {
+    if (!Array.isArray(data)) {
+        data = [data];
+    }
+    return data;
+};
+
+/**
+ * Trim empty array elements
+ * Note that current UTIL.log function won't reflect the changes due to a bug in JSON.stringify
+ * @param {Array} array - Array to trim
+ * @returns {Array} Trimmed array
+ */
+exports.trimArray = function(array) {
+    // Make sure array is an array
+    array = exports.forceArray(array);
+    var trimmedArray = [];
+    for (var i = 0; i < array.length; i++) {
+        var empty = true;
+        var object = array[i];
+
+        for (var key in object) {
+            if (object[key] !== '') {
+                empty = false;
+            }
+        }
+        if (!empty) {
+            trimmedArray.push(object);
+        }
+    }
+    return trimmedArray;
+};
+
+/**
+ * Delete all properties with empty string from an object
+ * Set 'recursive' to true if you also want to delete properties in nested objects
+ * @param {Object} obj
+ * @param {boolean} recursive
+ */
+exports.deleteEmptyProperties = function(obj, recursive) {
+    for (var i in obj) {
+        if (obj[i] === '') {
+            delete obj[i];
+        } else if (recursive && typeof obj[i] === 'object') {
+            exports.deleteEmptyProperties(obj[i], recursive);
+        }
+    }
+};
+
+/**
+ * Check if value is integer
+ * @param value
+ * @returns {boolean}
+ */
+exports.isInt = function(value) {
+    return !isNaN(value) &&
+        parseInt(Number(value)) == value &&
+        !isNaN(parseInt(value, 10));
+};
--- src/main/resources/site/parts/menu/menu.js
+++ src/main/resources/site/parts/menu/menu.js
@@ -1,9 +1,33 @@
 exports.get = function (req) {
 
-    var portal = require('/lib/xp/portal');
     var thymeleaf = require('/lib/xp/thymeleaf');
+    var contentLib = require('/lib/xp/content');
+    var data = require('/lib/data');
 
-    var model = {};
+    var pizzas = contentLib.getChildren({
+        key: '/ankis-bar/dishes/pizza',
+        count: 1000000
+    }).hits.map(function (hit) {
+        var ingredientKeys = hit.data.ingredients || [];
+        ingredientKeys = data.forceArray(ingredientKeys);
+        var ingredients = ingredientKeys.map(function (key) {
+            var hit = contentLib.get({
+                key: key,
+            });
+            return {
+                key: hit.data.key
+            };
+        });
+        return {
+            key: hit.data.key,
+            ingredients: ingredients
+        }
+    });
+
+    var model = {
+        selectedLocale: req.cookies.locale || 'se',
+        pizzas: pizzas
+    };
 
     var view = resolve('menu.html');
 
